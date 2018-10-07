08a63c915b0d3d67c0e27053408efbf7f0fa0281
Kevin Cajachuan <kevincajachuan@hotmail.com>
2018-10-07 14:29:59 -0300

    Iluminación del cubo lista

--- tp/script.js
+++ tp/script.js
@@ -107,7 +107,7 @@ function getShader(gl, id) {
 }
 
 function setupBuffers() {
-  o = new Ball(0,0,1);
+  o = new Box(0,0,1);
   o.setupWebGLBuffers();
   m = mat4.create();
   o.localMatrix = m;
@@ -196,7 +196,7 @@ function drawScene() {
   var ambient_color = gl.getUniformLocation(glProgram, "uAmbientColor");
 
   var lighting_direction = gl.getUniformLocation(glProgram, "uLightPosition");
-  var lightPosition = [0, 5, 5];
+  var lightPosition = [0, 3, 4];
   gl.uniform3fv(lighting_direction, lightPosition);
 
   var directional_color = gl.getUniformLocation(glProgram, "uDirectionalColor");
@@ -284,17 +284,26 @@ function Box(r, g, b) {
   this.g = g;
   this.b = b;
 
-  this.position_buffer = [1,  1,  1,  -1, 1,  1,  -1, -1,  1,   1, -1,  1,
-                          1, -1, -1,   1, 1, -1,  -1,  1, -1,  -1, -1, -1];
+  this.position_buffer = [1,1,1,  -1,1,1,  -1,-1,1,  1,-1,1,
+                          1,1,1,  1,-1,1,  1,-1,-1,  1,1,-1,
+                          1,1,1,  1,1,-1,  -1,1,-1,  -1,1,1,
+                          -1,1,1,  -1,1,-1,  -1,-1,-1,  -1,-1,1,
+                          -1,-1,1,  -1,-1,-1,  1,-1,-1,  1,-1,1,
+                          -1,-1,-1,  -1,1,-1,  1,1,-1,  1,-1,-1];
 
-  this.normal_buffer = this.position_buffer;
+  this.normal_buffer = [0,0,1,  0,0,1,  0,0,1,  0,0,1,
+                        1,0,0,  1,0,0,  1,0,0,  1,0,0,
+                        0,1,0,  0,1,0,  0,1,0,  0,1,0,
+                        -1,0,0,  -1,0,0,  -1,0,0,  -1,0,0,
+                        0,-1,0,  0,-1,0,  0,-1,0,  0,-1,0,
+                        0,0,-1,  0,0,-1,  0,0,-1,  0,0,-1];
 
-  this.index_buffer = [0, 1, 2,  0, 2, 3,
-                       0, 3, 4,  0, 4, 5,
-                       0, 5, 6,  0, 6, 1,
-                       6, 1, 7,  7, 1, 2,
-                       7, 2, 3,  7, 3, 4,
-                       4, 7, 6,  4, 6, 5];
+  this.index_buffer = [0,1,2,  0,2,3,  3,0,4,
+                       7,4,5,  7,5,6,  6,7,9,
+                       11,8,9,  11,9,10,  10,11,12,
+                       15,12,13,  15,13,14, 14,15,16,
+                       19,16,17,  19,17,18,  19,18,23,
+                       23,20,21,  23,21,22];
 
   Node.call(this);
 }
