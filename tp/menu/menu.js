0ac231298aa09d3a1a1ece2b775f35236948c1e6
Kevin Cajachuan <kevincajachuan@hotmail.com>
2018-10-27 21:46:02 -0300

    Agrego nombres al menú (Todavía no funciona)

--- tp/menu/menu.js
+++ tp/menu/menu.js
@@ -1,56 +1,42 @@
-
 var menu = {
-	alturaMaxima:10,
 	reiniciar:function(){
 		alert("Se reiniciaron los parámetros");
 	},
-	detener:function(){
-		alert("Se detuvo la ejecución");
-	},
-	modo:"random",
-	ancho:0,
-	umbral:100,
-	samples:2
 };
 
-function dummy(){
-	console.log("dummy function called");
-}
-
-var cantidadTotal=5;
+radioTotal = 2;
+altura = 2;
+ciclos = 2;
+vueltas = 2;
+decorador = "Bola";
+cantidadDecoradores = 2;
+contorno = "Tubo";
+cantidadContorno = 2;
 
 function GUI (){
-		// creamos el menu
 
 		var gui = new dat.GUI();
 
-		gui.add(window,"cantidadTotal",0,10);
+		gui.add(menu,"reiniciar").name("Reiniciar"); // Este debe ser el botón reiniciar que aun no funciona
 
-		// definimos una carpeta comandos en la variable f1
-		var f1 = gui.addFolder('Comandos');
+		var f1 = gui.addFolder('Base');
+		f1.add(window, 'radioTotal', 1, 10).name("Radio");
+		f1.add(window, 'altura', 1, 5).name("Altura");
+		f1.add(window, 'ciclos', 1, 5).name("Ciclos");
 
-		//el metodo add recibe un objeto y el nombre del atributo o funcion en forma de STRING
-		f1.add(menu, 'reiniciar').name("Reiniciar");
-		f1.add(menu, 'detener').name("detener");
+		var f2 = gui.addFolder('Anillo');
+		f2.add(window, 'vueltas', 1, 6).name("Torsión");
 
-		// si se desea invocar a una funcion o una variable global, se pasa el parametro window como primer argumento
-		f1.add(window, 'dummy').name("detener");
+		var f3 = gui.addFolder('Decoradores');
+		f3.add(window, 'decorador', ["Bola", "Campana", "Paleta"]).name("Tipo");
+		f3.add(window, 'cantidadDecoradores', 1, 10).name("Cantidad");
 
-		f1.open(); // hace que la carpeta f1 inicie abierta
+		var f4 = gui.addFolder('Contorno');
+		f4.add(window, 'contorno', ["Tubo", "Barra"]).name("Tipo");
+		f4.add(window, 'cantidadContorno', 2, 20).name("Cantidad");
 
-		var f2 = gui.addFolder('Parametros generales');
-
-		f2.add(menu, 'alturaMaxima', 1.0, 60.0).name("altura maxima").step(1);
-		f2.add(menu, 'ancho',4,25).name("Ancho");
-
-		f2.add(menu, 'modo',["random","secuencial"]).name("modo");
-
-		var f3 = gui.addFolder('Parametros Especiales ');
-		f3.add(menu,'umbral',0.0,200.0).name("umbral");
-		f3.add(menu,'samples',0,30).name("samples").onChange(function(v){
-			console.log(" cambio el valor de app.samples a "+v);
-		});
-
+		f1.open();
 		f2.open();
 		f3.open();
+		f4.open();
   };
