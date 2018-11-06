3c1346709de97ac8be335b510acc1f56058d9c64
Kevin Cajachuan <kevincajachuan@hotmail.com>
2018-11-06 12:20:27 -0300

    Reordeno funciones

--- tp/script.js
+++ tp/script.js
@@ -392,6 +392,16 @@ function setupBuffers() {
   // objects[offset + 4].updateWorldMatrix();
 }
 
+function handleLoadedTexture(texture, image) {
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+  gl.generateMipmap(gl.TEXTURE_2D);
+
+  gl.bindTexture(gl.TEXTURE_2D, null);
+}
 
 /*
 Logica de animación:
@@ -559,21 +569,6 @@ Node.prototype.initTexture = function(path) {
   this.texture.image.src = path;
 }
 ///////////////////////////////////////////////////////////////////////////////
-
-function handleLoadedTexture(texture, image) {
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-  gl.generateMipmap(gl.TEXTURE_2D);
-
-  gl.bindTexture(gl.TEXTURE_2D, null);
-}
-
-function isPowerOf2(n) {
-  return (n & (n - 1)) == 0;
-}
 
 // Clase Box
 function Box(r, g, b) {
