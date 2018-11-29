927770650dd31bb17d0108d5c031449e838e7a11
FabrizioCozza <FabrizioCozza@users.noreply.github.com>
2018-11-29 03:10:29 -0300

    Parte de animacion estacion 2

--- tp/script.js
+++ tp/script.js
@@ -269,7 +269,7 @@ function setupBuffers() {
   box2Station2 = new Box(0.882, 0.667, 0.416);
   box2Station2.setupWebGLBuffers();
   mB2s2 = mat4.create();
-  mat4.translate(mB2s2, mB2s2, vec3.fromValues(-5, 2.85, -2.5));
+  mat4.translate(mB2s2, mB2s2, vec3.fromValues(-5, 2.4, -2.5));
   mat4.scale(mB2s2, mB2s2, vec3.fromValues(0.05, 0.25, 0.5));
   box2Station2.localMatrix = mB2s2;
   box2Station2.setParent(box1Station2);
@@ -279,7 +279,7 @@ function setupBuffers() {
   box3Station2 = new Box(0.882, 0.667, 0.416);
   box3Station2.setupWebGLBuffers();
   mB2s2 = mat4.create();
-  mat4.translate(mB2s2, mB2s2, vec3.fromValues(-5, 2.85, -2.1));
+  mat4.translate(mB2s2, mB2s2, vec3.fromValues(-5, 2.4, -2.1));
   mat4.scale(mB2s2, mB2s2, vec3.fromValues(0.1, 0.3, 0.1));
   box3Station2.localMatrix = mB2s2;
   box3Station2.setParent(box2Station2);
@@ -366,7 +366,7 @@ function setupBuffers() {
     paramCont.push("Wafer", 0.02, 0.2, 0.05);
 
   mContours = mat4.create();
-  mat4.translate(mContours, mContours, vec3.fromValues(-5, 2.9, -1.96));
+  mat4.translate(mContours, mContours, vec3.fromValues(-5, 2.42, -1.96));
   mat4.scale(mContours, mContours, vec3.fromValues(paramCont[1], paramCont[2], paramCont[3]));
 
   var cont = [];
@@ -460,7 +460,7 @@ function station1(){
   if (distance > 299) {
     distance = 0;
     contador = 0;
-    animationLoop(station1HookMoveToDecorator);
+    animationLoop(station2);
     return false;
   }
 }
@@ -817,7 +817,65 @@ function station2(){
   distance += 1;
   if (distance > 277) {
     distance = 0;
-    animationLoop(bandFinal);
+    c = 0;
+    animationLoop(station2DeployContorno);
+    return false;
+  }
+}
+
+function station2DeployContorno(){
+  mat4.scale(objects[16].localMatrix,objects[16].localMatrix, vec3.fromValues(1, 1, 1.028));
+  mat4.translate(objects[17].localMatrix,objects[17].localMatrix,vec3.fromValues(0,0,horizontalVelocity*11));
+  mat4.translate(objects[21 + cantidadDecoradores + c].localMatrix,objects[21 + cantidadDecoradores + c].localMatrix,vec3.fromValues(0,0,horizontalVelocity*12));
+  objects[16].updateWorldMatrix();
+  objects[17].updateWorldMatrix();
+  objects[21 + cantidadDecoradores + c].updateWorldMatrix();
+  distance += 1;
+  if (distance > 30) {
+    distance = 0;
+    animationLoop(station2DeployContorno2);
+    return false;
+  }
+}
+
+function station2DeployContorno2(){
+  mat4.scale(objects[16].localMatrix,objects[16].localMatrix, vec3.fromValues(1, 1, 0.97274));
+  mat4.translate(objects[17].localMatrix,objects[17].localMatrix,vec3.fromValues(0,0,-horizontalVelocity*11));
+  objects[16].updateWorldMatrix();
+  objects[17].updateWorldMatrix();
+  distance += 1;
+  if (distance > 30) {
+    distance = 0;
+    animationLoop(station2RotateCakeAndContornos);
+    return false;
+  }
+}
+
+function degToRad(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+
+function station2RotateCakeAndContornos(){
+  if(c < cantidadContorno){
+    var degreesToMove = degToRad(1);
+    mat4.rotate(objects[18].localMatrix,objects[18].localMatrix,degreesToMove,vec3.fromValues(0,1,0));
+    mat4.rotate(objects[19].localMatrix,objects[19].localMatrix,degreesToMove,vec3.fromValues(0,1,0));
+    mat4.rotate(objects[20].localMatrix,objects[20].localMatrix,degreesToMove,vec3.fromValues(0,1,0));
+    objects[18].updateWorldMatrix();
+    objects[19].updateWorldMatrix();
+    objects[20].updateWorldMatrix();
+  }
+  distance += 1;
+  if (distance > ((360/cantidadContorno) - 1)) {
+    distance = 0;
+    if (c < (cantidadContorno - 1)){
+      c += 1;
+      animationLoop(station2DeployContorno);
+    } else {
+      animationLoop(bandFinal);
+    }
     return false;
   }
 }
@@ -1039,7 +1097,7 @@ function setupBuffersStatic() {
   box2Station2 = new Box(0.882, 0.667, 0.416);
   box2Station2.setupWebGLBuffers();
   mB2s2 = mat4.create();
-  mat4.translate(mB2s2, mB2s2, vec3.fromValues(-5, 2.85, -2.5));
+  mat4.translate(mB2s2, mB2s2, vec3.fromValues(-5, 2.4, -2.5));
   mat4.scale(mB2s2, mB2s2, vec3.fromValues(0.05, 0.25, 0.5));
   box2Station2.localMatrix = mB2s2;
   box2Station2.setParent(box1Station2);
@@ -1049,7 +1107,7 @@ function setupBuffersStatic() {
   box3Station2 = new Box(0.882, 0.667, 0.416);
   box3Station2.setupWebGLBuffers();
   mB2s2 = mat4.create();
-  mat4.translate(mB2s2, mB2s2, vec3.fromValues(-5, 2.85, -2.1));
+  mat4.translate(mB2s2, mB2s2, vec3.fromValues(-5, 2.4, -2.1));
   mat4.scale(mB2s2, mB2s2, vec3.fromValues(0.1, 0.3, 0.1));
   box3Station2.localMatrix = mB2s2;
   box3Station2.setParent(box2Station2);
